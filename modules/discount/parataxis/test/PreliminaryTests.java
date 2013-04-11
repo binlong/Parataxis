c4c4a3cd937637b53fb04d1db652474130bc0d35
Randy J. Weber <rweber@cs.utsa.edu>
2013-04-11 18:16:01 -0500

    more coupon shit

---
+++ modules/discount/parataxis/test/PreliminaryTests.java
@@ -0,0 +1,46 @@
+package discount.parataxis.test;
+
+import init.parataxis.main.PopulateCoupon;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import parataxis.dto.Coupon;
+import parataxis.dto.Grocery;
+import scan.parataxis.main.Scan;
+
+/**
+ * This class is just to test some different classes up to this point
+ * NOTE: This class is not to be used for the actual testing. DELETE ME
+ * @author rweber
+ *
+ */
+public class PreliminaryTests {
+
+	
+	public static void main(String[] args) throws IOException {
+		Scan scn = new Scan();
+		ArrayList<Grocery> scanList = scn.scanItems(); 
+		
+		PopulateCoupon popcpn = new PopulateCoupon();
+		ArrayList<Coupon> cpnList = popcpn.populateCouponList();
+		
+		
+		for(Grocery grc : scanList){
+			grc.getUpc();
+			for(Coupon cpn : cpnList){
+				if(grc.getUpc().equals(cpn.getUpc())){
+					if(cpn.getType() == 'S')
+						System.out.print("Store");
+					if(cpn.getType() == 'M')
+						System.out.print("Cents-off Manufacturer");
+					if(cpn.getType() == 'X')
+						System.out.print("Buy "+cpn.getBuyM()+" Get "+cpn.getGetN());
+					System.out.println(" coupon found for: "+grc.getUpc());
+				}
+			}
+		}
+
+	}
+
+}

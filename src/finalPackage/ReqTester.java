fce1a36c74eab3213d21289612778df50cabd250
Sam Mangum <smangum@cs.utsa.edu>
2013-04-16 21:26:26 -0500

    added finalPackage/ReqTester.java

---
+++ src/finalPackage/ReqTester.java
@@ -0,0 +1,244 @@
+package finalPackage;
+
+import init.parataxis.main.PopulateCustomers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import parataxis.dto.Customer;
+
+public class ReqTester {
+
+	
+	public static void main(String[] args) {
+		
+	ArrayList<Customer> testCustomers =new ArrayList<Customer>();
+	PopulateCustomers p = new PopulateCustomers();
+	
+	try {
+		testCustomers = p.populateCustomerList();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+	for(Customer cust:testCustomers)
+	System.out.println(cust);
+	}
+	
+	public static void tc00001(){
+		/* later */
+	}
+	public static void tc00002(){
+		/* later */
+	}
+	public static void tc00003(){
+		/* later */
+	}
+	public static void tc00004(){
+		/* later */
+	}
+	public static void tc00005(){
+		/* later */
+	}
+	public static void tc00006(){
+		/* later */
+	}
+	public static void tc00007(){
+		/* later */
+	}
+	public static void tc00008(){
+		/* later */
+	}
+	public static void tc00009(){
+		/* later */
+	}
+	public static void tc00010(){
+		/* later */
+	}
+	public static void tc00011(){
+		/* later */
+	}
+	public static void tc00012(){
+		/* later */
+	}
+	public static void tc00013(){
+		/* later */
+	}
+	public static void tc00014(){
+		/* later */
+	}
+	public static void tc00015(){
+		/* later */
+	}
+	public static void tc00016(){
+		/* later */
+	}
+	public static void tc00017(){
+		/* later */
+	}
+	public static void tc00018(){
+		/* later */
+	}
+	public static void tc00019(){
+		/* later */
+	}
+	public static void tc00020(){
+		/* later */
+	}
+	public static void tc00021(){
+		/* later */
+	}
+	public static void tc00022(){
+		/* later */
+	}
+	public static void tc00023(){
+		/* later */
+	}
+	public static void tc00024(){
+		/* later */
+	}
+	public static void tc00025(){
+		/* later */
+	}
+	public static void tc00026(){
+		/* later */
+	}
+	public static void tc00027(){
+		/* later */
+	}
+	public static void tc00028(){
+		/* later */
+	}
+	public static void tc00029(){
+		/* later */
+	}
+	public static void tc00030(){
+	    /* later */
+	}
+	public static void tc00031(){
+		/* later */
+	}
+	public static void tc00032(){
+		/* later */
+	}
+	public static void tc00033(){
+		/* later */
+	}
+	public static void tc00034(){
+		/* later */
+	}
+	public static void tc00035(){
+		/* later */
+	}
+	public static void tc00036(){
+		/* later */
+	}
+	public static void tc00037(){
+		/* later */
+	}
+	public static void tc00038(){
+		/* later */
+	}
+	public static void tc00039(){
+		/* later */
+	}
+	public static void tc00040(){
+		/* later */
+	}
+	public static void tc00041(){
+		/* later */
+	}
+	public static void tc00042(){
+		/* later */
+	}
+	public static void tc00043(){
+		/* later */
+	}
+	public static void tc00044(){
+		/* later */
+	}
+	public static void tc00045(){
+		/* later */
+	}
+	public static void tc00046(){
+		/* later */
+	}
+	public static void tc00047(){
+		/* later */
+	}
+	public static void tc00048(){
+		/* later */
+	}
+	public static void tc00049(){
+		/* later */
+	}
+	public static void tc00050(){
+		/* later */
+	}
+	public static void tc00051(){
+		/* later */
+	}
+	public static void tc00052(){
+		/* later */
+	}
+	public static void tc00053(){
+		/* later */
+	}
+	public static void tc00054(){
+		/* later */
+	}
+	public static void tc00055(){
+		/* later */
+	}
+	public static void tc00056(){
+		/* later */
+	}
+	public static void tc00057(){
+		/* later */
+	}
+	public static void tc00058(){
+		/* later */
+	}
+	public static void tc00059(){
+		/* later */
+	}
+	public static void tc00060(){
+		/* later */
+	}
+	public static void tc00061(){
+		/* later */
+	}
+	public static void tc00062(){
+		/* later */
+	}
+	public static void tc00063(){
+		/* later */
+	}
+	public static void tc00064(){
+		/* later */
+	}
+	public static void tc00065(){
+		/* later */
+	}
+	public static void tc00066(){
+		/* later */
+	}
+	public static void tc00067(){
+		/* later */
+	}
+	public static void tc00068(){
+		/* later */
+	}
+	public static void tc00069(){
+		/* later */
+	}
+	public static void tc00070(){
+		/* later */
+	}
+	public static void tc00071(){
+		/* later */
+	}
+	
+	
+
+}

070644eabba309f07a14b210303c2911e4eda425
Randy J. Weber <randyweber1989@yahoo.com>
2013-04-29 18:25:38 -0500

    Changed unit tests for scan classes

--- modules/scan/parataxis/test/ScanTest.java
+++ modules/scan/parataxis/test/ScanTest.java
@@ -3,9 +3,13 @@ package scan.parataxis.test;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 
+import parataxis.dto.Basket;
 import parataxis.dto.Coupon;
 import parataxis.dto.Grocery;
+import parataxis.dto.Receipt;
+import parataxis.dto.Tax;
 import scan.parataxis.main.Scan;
 
 
@@ -14,12 +18,44 @@ public class ScanTest {
 	final static String testFilename = "smallFile.txt";
 	
 	public static void main(String[] args) throws IOException, ParseException{
+		//NOTE: in order to properly test if scan works, we need to print out\
+		// the data scanned via the receipt class, since scan populates the data
+		// while receipt prints out this data.
+		
 		System.out.println("[TEST] Entering ScanTest main method");
 		
 		Scan scn = new Scan(testFilename);
 		System.out.println("[TEST] Scan initialized using file: "+scn.getFile_TEST());
 		
+		Scan scan = new Scan("SampleInputLarge.txt");
+		Receipt receipt = null;
+		ArrayList<Basket> blist = null;
+		try {
+			blist = scan.scanFile();
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(Basket b:blist){
+
+
+			Tax tax = new Tax(7.7, new Date(), new Date());
+			try{
+			if(b.getPaymentType().equals("card")){
+				receipt  = new Receipt(b.getDate(), b.getItemBasket(), b.getCustomer(), tax, b.getCashback(), b.getCouponList());
+			} else if (b.getPaymentType().equals("cash")){
+				receipt = new Receipt(b.getDate(), b.getItemBasket(), b.getAmountPaid(), tax, b.getCouponList());
+			}
+			
+			System.out.println();
+			System.out.println(receipt);
+			System.out.println();
+			}catch(NullPointerException e){
+				System.out.println("\nEmpty Basket\n");
+			}
+		}
 		
+		/*
 		ArrayList<Grocery> testlist = scn.getGroceryList();
 		System.out.println("[TEST] testlist initialized via scanItems() | testlist.size()="+testlist.size());
 		
@@ -38,5 +74,6 @@ public class ScanTest {
 		}
 		
 		System.out.println("[TEST] Exiting ScanTest main method successfully");
+		*/
 	}
 }
